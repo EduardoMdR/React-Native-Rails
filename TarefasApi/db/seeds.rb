e4818d2688dfcbb97af6c8c14b2377827dcbb53b
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-11-28 00:23:00 -0300

    Inicio do React native

---
+++ TarefasApi/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(title: 'Continuar o curso de pixel art ', done: false)
+Task.create(title: 'Arrumar o drive (onedrive, email unb e meu proprio)', done: false)
+Task.create(title: 'Aprender JavaScript e Aprender python', done: false)
+Task.create(title: 'Arrumar o drive (onedrive, email unb e meu proprio)', done: false)
+Task.create(title: 'Realizar ProjetoOnde', done: true)
